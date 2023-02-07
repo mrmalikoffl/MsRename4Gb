3df664e8e9dfe875158ca8846e9d476571f0df4f
Mr Malik <121479735+mrmalikoffl@users.noreply.github.com>
2023-02-07 14:40:10 +0800

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"👉🏻Total User:- {total_user()}\n👉🏻Developer :- @mrmalik_offl\n👉🏻Buy Subscription :- @mrmalik_offl\n👉🏻Bot Updates :- https://youtube.com/@GreyMattersYT\n\n👉🏻Total Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"👉🏻𝙏𝙤𝙩𝙖𝙡 𝙐𝙨𝙚𝙧𝙨: {total_user()}\n\n👉🏻𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧: @mrmalik_offl\n\n👉🏻𝙁𝙤𝙧 𝙋𝙡𝙖𝙣 𝙐𝙥𝙜𝙧𝙖𝙙𝙚: @mrmalik_offl\n\n👉🏻𝘽𝙤𝙩 𝙐𝙥𝙙𝙖𝙩𝙚𝙨: https://t.me/+rN9QCFgIihgyZWM1\n\n👉🏻𝙏𝙤𝙩𝙖𝙡 𝙍𝙚𝙣𝙖𝙢𝙚𝙙 𝙁𝙞𝙡𝙚𝙨: {total_rename}\n\n𝙏𝙤𝙩𝙖𝙡 𝙎𝙞𝙯𝙚 𝙍𝙚𝙣𝙖𝙢𝙚𝙙: {humanbytes(int(total_size))}\n\n𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠 𝐌𝐞",quote=True)
